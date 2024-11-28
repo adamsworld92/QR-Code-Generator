580212d5aed1f33b7f571231e8563f7affb7bc7f
Adams World <adamsworld1992@gmail.com>
2024-11-28 16:04:05 +0000

    Test passed for QR Code Class Initialize

--- src/qr_generator.py
+++ src/qr_generator.py
@@ -1,1 +1,17 @@
+import qrcode
+from PIL import Image
+from pathlib import Path
 
+class QRCodeGenerator:
+
+    def __init__(self, version=1, box_size=10, border=4):
+
+        self.version = version
+        self.box_size = box_size
+        self.border = border
+
+        self.qr = qrcode.QRCode(
+            version=version,
+            box_size=box_size,
+            border=border
+        )
